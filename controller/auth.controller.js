2a0d25a88ff449b2198fc06789eb07465d31d767
EnTeVi <andriy.lytvynchuk@gmail.com>
2022-12-20 16:13:54 +0200

    Initial commit

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,8 @@
 const oauthService = require("../service/oauth.service");
 const emailService = require("../service/email.service");
+const smsService = require("../service/sms.service");
+const { smsActionTypeEnum } = require("../enum");
+const smsTemplate = require("../helper/sms-template.helper");
 const ActionToken = require("../dataBases/ActionToken");
 const OldPassword = require("../dataBases/OldPassword");
 const OAuth = require("../dataBases/OAuth");
@@ -13,9 +16,12 @@ module.exports = {
         try {
             const { user, body } = req;
 
-            await emailService.sendEmail(user.email, WELCOME, { userName: user.name, array: [{ number: 1}, { number: 2}, { number: 3}], condition: false });
+            await Promise.allSettled([
+                emailService.sendEmail(user.email, WELCOME, { userName: user.name, array: [{ number: 1 }], condition: false }),
+                smsService.sendSms(smsTemplate[smsActionTypeEnum.WELCOME](user.name), user.phone)
+            ]);
 
-            await oauthService.comparePasswords(user.password, body.password);
+            await user.comparePasswords(body.password);
 
             const tokenPair = oauthService.generateAccessTokenPair({ id: user._id });
 
@@ -68,7 +74,7 @@ module.exports = {
 
             const hashPassword = await oauthService.hashPassword(body.password);
 
-            await OldPassword.create({ _user_id: user._id, password: user.password});
+            await OldPassword.create({ _user_id: user._id, password: user.password });
 
             await ActionToken.deleteOne({ token: req.get('Authorization') });
             await User.updateOne({ _id: user._id }, { password: hashPassword });
